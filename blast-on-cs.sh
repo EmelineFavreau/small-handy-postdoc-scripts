758582de11fc3aa7c7cc86559ded4e9ec97f3538
Emeline Favreau <9661216+EmelineFavreau@users.noreply.github.com>
2021-08-16 16:40:23 +0100

    Create blast-on-cs.sh

---
+++ blast-on-cs.sh
@@ -0,0 +1,1 @@
+/share/apps/genomics/blast-2.10.0+/bin/blastp -query tmp/V_crabro-protein-and-sequences-to-blast.faa -entrez_query "honey bee[organism]" -db nr -remote -max_target_seqs 1 -outfmt 6 -out result/duplication-analysis/V_crabro-blast-result
