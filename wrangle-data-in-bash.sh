452e578c1bd167ca178777db8120732462f32b19
Emeline Favreau <9661216+EmelineFavreau@users.noreply.github.com>
2021-10-01 14:52:29 +0100

    Update wrangle-data-in-bash.sh

--- wrangle-data-in-bash.sh
+++ wrangle-data-in-bash.sh
@@ -4,3 +4,9 @@ for species in $(cat ../species_list); do
 	thisspecies="${species}"
 	awk '$5 == "Terminal" { print $0}' orthogroups-analysis/Results_Aug12_1/Gene_Duplication_Events/Duplications.tsv | awk '$2 == "'$thisspecies'-longest-isoforms"' | cut -f 1 | sort | uniq > duplication-analysis/${species}_orthogroups_in_duplication_events
 done
+
+# Assess a large amount of files and check all are there
+# for each orthogroup, check if the file exists, if not print the orthogroup
+while read orthogroup; do
+   test -f result/calculating-dnds/branch-model/${orthogroup} || echo $orthogroup
+done < tmp/aligned-orthogroups
